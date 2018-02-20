a2b866a2bbaafc4aca331b980f4a1c7107c2c264
Nick-Hericks <nickhericks@gmail.com>
2018-02-20 12:38:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Memory Game (Udacity project)
+
+This project is part of Udacity's Front End Web Developer nanodegree program. Demonstrates knowledge of HTML, CSS, Bootstrap framework, and Javascript. Game idea provided by Udacity. All code written by me.
+
---
+++ js/app.js
@@ -0,0 +1,33 @@
+// Ensure JS runs after all HTML is loaded
+// All JavaScript goes inside this handler
+document.addEventListener('DOMContentLoaded', () => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Close DOMContentLoaded handler
+// All JavaScript goes above this closing bracket
+})
